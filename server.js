77eb7b4940c06fdf3c037f06c1fc1800b4160817
Prince Owusu <princeowusu.272@gmail.com>
2022-12-06 10:57:03 +0000

    Commitcccc

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const { createServer } = require("http");
+const { createServer } = require("https");
 const next = require("next");
-
+const { parse } = require("url");
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    handle(req, res);
+  createServer({}, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
   }).listen(port, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+    console.log(`> Ready on https://localhost:${port}`);
   });
 });
