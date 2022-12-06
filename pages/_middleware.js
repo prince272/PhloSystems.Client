05d4f94c8e442bb92e4fda723d593059373c3a9b
Prince Owusu <princeowusu.272@gmail.com>
2022-12-06 11:45:55 +0000

    Commit.

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,14 +4,13 @@ import { findContextualRoute, PAGE_PATH_QUERY_PARAM } from '../views/routes';
 function middleware(request) {
 
     try {
+        console.error(JSON.stringify(request));
+        console.error('JSON.stringify(request) done');
         if (findContextualRoute(request.nextUrl.toString())) { 
             const pagePath = request.nextUrl.searchParams.get(PAGE_PATH_QUERY_PARAM) || '/';
             return NextResponse.rewrite(new URL(pagePath, request.nextUrl));
         } 
     }
     catch(error) {}
-
-    const response = NextResponse.next();
-    return response;
 }
 export default middleware;
