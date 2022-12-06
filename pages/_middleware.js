13ab75da38011cdfd60f17e200aa42864e030ee5
Prince Owusu <princeowusu.272@gmail.com>
2022-12-06 11:06:20 +0000

    cc

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,11 +3,16 @@ import { findContextualRoute, PAGE_PATH_QUERY_PARAM } from '../views/routes';
 
 function middleware(request) {
 
+    console.log('1');
+
     if (findContextualRoute(request.nextUrl.toString())) { 
+        console.log('2');
         const pagePath = request.nextUrl.searchParams.get(PAGE_PATH_QUERY_PARAM) || '/';
+        console.log('3');
         return NextResponse.rewrite(new URL(pagePath, request.nextUrl));
     } 
 
+    console.log('4');
     const response = NextResponse.next();
     return response;
 }
