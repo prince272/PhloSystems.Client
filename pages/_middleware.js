edad16128b8bde02637e5fce30e17d36e7adb08e
Prince Owusu <princeowusu.272@gmail.com>
2022-12-06 11:31:50 +0000

    Com4

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,21 +1,16 @@
 import { NextResponse } from 'next/server';
-import { getPath } from '../utils';
 import { findContextualRoute, PAGE_PATH_QUERY_PARAM } from '../views/routes';
 
 function middleware(request) {
 
-    const middlewarePath = getPath(request.nextUrl?.href) || '/';
-
-    console.log('Testing' + middlewarePath);
-
-    if (middlewarePath && findContextualRoute(middlewarePath)) {
-        console.error('2');
-        const pagePath = request.nextUrl.searchParams.get(PAGE_PATH_QUERY_PARAM) || '/';
-        console.error('3');
-        return NextResponse.rewrite(new URL(pagePath, request.nextUrl.url));
+    try {
+        if (findContextualRoute(request.nextUrl.toString())) { 
+            const pagePath = request.nextUrl.searchParams.get(PAGE_PATH_QUERY_PARAM) || '/';
+            return NextResponse.rewrite(new URL(pagePath, request.nextUrl));
+        } 
     }
+    catch(error) {}
 
-    console.error('4');
     const response = NextResponse.next();
     return response;
 }
